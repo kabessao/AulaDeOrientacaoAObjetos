e3d2a2357aa5143defdfcc828379cd9ecea0af3c
Henrique Soares da Silva <henrique.silva533@etec.sp.gov.br>
2018-03-23 20:33:51 -0300

    Adicionado um exemplo de orientação a objetos

---
+++ AplicacaoFinanceira/AplicacaoFinanceira.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AplicacaoFinanceira
+{
+    class AplicacaoFinanceira
+    {
+        static void Main(string[] args)
+        {
+            Banco banco1 = new Banco();
+            Agencia agencia1 = new Agencia();
+            Conta conta1 = new Conta();
+            Console.WriteLine(banco1);
+            Console.WriteLine(agencia1);
+            Console.WriteLine(conta1);
+        }
+    }
+}
