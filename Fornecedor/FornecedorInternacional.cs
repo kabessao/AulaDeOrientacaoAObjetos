5c2d7292590ff13fc5041eb87b36b713da21c608
Henrique Soares da Silva <henrique.silva533@etec.sp.gov.br>
2018-05-30 22:08:53 -0300

    Algumas mudanças

---
+++ Fornecedor/FornecedorInternacional.cs
@@ -0,0 +1,27 @@
+﻿namespace Fornecedor
+{
+    class Fornecedor : Contato
+    {
+        private float valorCredito, valorDivida;
+
+        public virtual  float ValorCredito 
+        {
+            get { return valorCredito;}
+            set { valorCredito = value; }
+        }
+
+        public virtual float ValorDivida
+        {
+            get { return valorDivida; }
+            set { valorDivida = value; }
+        }
+        public Fornecedor(string nome, int idade, string telefone, float valorCredito, float valorDivida) : 
+            base(nome,idade,telefone)
+        {
+            valorCredito = valorCredito;
+            valorDivida = valorDivida;
+        }
+
+        public float Saldo() => ValorCredito - ValorDivida;
+    }
+}
